0d0a30d8c2db29953f3d907891cf8d8451f13920
Yan Zhai <zhaiyan920@gmail.com>
2017-06-01 20:36:57 -0500

    something

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,11 @@
+bash pack.sh
 cd linux
 bash rebuild.sh
 cd ..
 docker build -t boot2docker .
 docker run boot2docker > boot2docker.iso
 
+source user1rc
 id=`glance image-list | grep docker | awk -F\| '{print $2}' | tr -d \ `
 glance image-delete $id
 glance image-create --file boot2docker.iso --container-format bare --disk-format iso --name docker
