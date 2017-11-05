b9f4063fd8dd1416bde6f8414d013c0ca439c2da
yanzhai <yanzhai@cs.wisc.edu>
2017-11-04 22:52:48 -0500

    not sure why it always not get execute properly

--- rebuild.sh
+++ rebuild.sh
@@ -15,5 +15,6 @@ openstack user list -v
 source user1rc
 id=`glance image-list | grep docker | awk -F\| '{print $2}' | tr -d \ `
 glance image-delete $id
+echo glance image-create --file boot2docker.iso --container-format bare --disk-format iso --name docker
 glance image-create --file boot2docker.iso --container-format bare --disk-format iso --name docker
 
