3e47c8434c2580acf978201c649621e457ef3e28
Yan Zhai <zhaiyan920@gmail.com>
2018-01-31 23:18:47 -0700

    utility quick commit

--- update.sh
+++ update.sh
@@ -1,10 +1,11 @@
 
 export SCRIPT_HOME=/local
-docker-machine rm -f v1 v2 v3
+docker-machine rm -f debug
 . user1rc
 nova keypair-add --pub-key $SCRIPT_HOME/data-population/key1.pub key1
 imageid=`glance image-list | grep docker | awk -F \| '{print $2}' | tr -d \ `
 nova container-update-policy --container-id 2 --template-id=tapcon --template-params "{\"image\": \"$imageid\"}"
-for n in 1 2 3; do
-bash $SCRIPT_HOME/data-population/launch.sh v$n 192.1.$n.0/24 2
-done
+#for n in 1 2 3; do
+#bash $SCRIPT_HOME/data-population/launch.sh v$n 192.1.$n.0/24 2
+#done
+bash $SCRIPT_HOME/data-population/launch.sh debug 192.1.5.0/24 2
