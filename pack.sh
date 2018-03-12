2bb74c92fc8348d02b5f029ab03536ccf2b53de7
Yan Zhai <zhaiyan920@gmail.com>
2018-03-12 01:41:29 -0500

    automate everything

--- pack.sh
+++ pack.sh
@@ -6,6 +6,6 @@ cp $GOPATH/src/github.com/docker/docker/bundles/latest/dynbinary-client/* docker
 cp $GOPATH/src/github.com/docker/docker/bundles/latest/dynbinary-daemon/* docker/
 rm docker/docker-1.14.0-dev
 rm docker/dockerd-1.14.0-dev
-tar czf tmpdocker.tgz docker/docker* docker/strace
+tar czf tmpdocker.tgz docker/docker* docker/strace docker/attguard*
 tar czf tmpdockerlib.tgz docker/lib*
 
