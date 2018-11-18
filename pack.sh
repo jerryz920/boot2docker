f688ffa15d06bd3ea6d94768dc9f338a90abba07
Yan Zhai <zhaiyan920@gmail.com>
2018-11-17 21:26:43 -0700

    utility quick commit

--- pack.sh
+++ pack.sh
@@ -4,6 +4,8 @@ cp /usr/bin/docker* docker/
 cp /usr/bin/strace docker/
 cp $GOPATH/src/github.com/docker/docker/bundles/latest/dynbinary-client/* docker/
 cp $GOPATH/src/github.com/docker/docker/bundles/latest/dynbinary-daemon/* docker/
+cp /usr/bin/attguard docker/
+cp /usr/bin/attguard-daemon docker/
 rm docker/docker-1.14.0-dev
 rm docker/dockerd-1.14.0-dev
 tar czf tmpdocker.tgz docker/docker* docker/strace docker/attguard*
